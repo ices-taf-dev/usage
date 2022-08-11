f04cc9aff85274a56adbbd8584e142abc2fec80a
Arni Magnusson <thisisarni@gmail.com>
2022-08-12 00:07:19 +1100

    Use lim() to set y limits

--- report.R
+++ report.R
@@ -9,8 +9,9 @@ mkdir("report")
 
 taf.png("assmt_proportion", width=1200, height=1200)
 tafstocks <- read.taf("output/tafstocks.csv")
-plot(Proportion~Year, data=tafstocks, subset=Categories=="Cat 1", ylim=lim,
-     yaxs="i", type="l", lwd=2, col="black", main="TAF stock assessments")
+plot(Proportion~Year, data=tafstocks, subset=Categories=="Cat 1",
+     ylim=lim(tafstocks$Proportion), yaxs="i", type="l", lwd=2, col="black",
+     main="TAF stock assessments")
 lines(Proportion~Year, data=tafstocks, subset=Categories=="Cat 2-6",
       lwd=2, lty=2)
 legend("topright", c("Cat 1","Cat 2-6"), lwd=2, lty=1:2,
